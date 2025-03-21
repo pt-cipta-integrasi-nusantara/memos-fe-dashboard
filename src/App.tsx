adb2d9d86bf9d3a291df694ce5c2d26f61390e59
adrianrivald <adrianrivaldy@gmail.com>
2025-03-21 09:21:20 +0700

    idle 10 minutes loggeed out

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { Router } from "./routes/sections";
 import { BottomMenuBar, Navbar, SideMenuBar } from "./components";
-import { Toaster } from "react-hot-toast";
+import { toast, Toaster } from "react-hot-toast";
 import { useAuth } from "./utils/auth/providers";
 import { useLocation } from "react-router-dom";
 
@@ -10,8 +10,35 @@ import { useLocation } from "react-router-dom";
 export default function App() {
   const location = useLocation();
   const asPath = location.pathname + location.search;
-  const { isAuth } = useAuth();
+  const { isAuth, logout } = useAuth();
   const [isExpandedMenubar, setIsExpandedMenubar] = React.useState(true);
+
+  // Auto logout if idling for 10 minutes
+  React.useEffect(() => {
+    let logoutTimer = 0;
+
+    const resetTimer = () => {
+      clearTimeout(logoutTimer);
+      logoutTimer = setTimeout(async () => {
+        await logout();
+        toast.success("Anda telah keluar karena idle selama 10 menit");
+      }, 600000); // 10 minutes (600000 ms)
+    };
+
+    // Listen to user interactions
+    window.addEventListener("mousemove", resetTimer);
+    window.addEventListener("keydown", resetTimer);
+
+    if (isAuth) {
+      resetTimer(); // Initialize the timer when component mounts
+    }
+
+    return () => {
+      clearTimeout(logoutTimer);
+      window.removeEventListener("mousemove", resetTimer);
+      window.removeEventListener("keydown", resetTimer);
+    };
+  }, [isAuth]);
   return (
     <>
       <head>
